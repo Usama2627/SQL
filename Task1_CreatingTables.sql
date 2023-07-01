7bf0c75e74e469d9963c119ed5c003c1b94558e1
Usama2627 <www.usamaattar2627@gmail.com>
2023-07-01 20:49:31 +0530

    Task1_CreatingTables

---
+++ Task1_CreatingTables.sql
@@ -0,0 +1,117 @@
+CREATE DATABASE my_familyInformation;
+use my_familyInformation;
+CREATE TABLE my_father(name varchar(20) , 
+                       age int , 
+                       occupation varchar(20) ,
+                       mobileNo bigint , 
+                       gmail varchar(30),
+                       height float ,
+                       weight float ,
+                       marital_status varchar(10) ,
+                       adhar_no bigint ,
+                       pan_no varchar(10));
+
+CREATE TABLE my_mother(name varchar(20) , 
+                       age int , 
+                       occupation varchar(20) ,
+                       mobileNo bigint , 
+                       gmail varchar(30),
+                       height float ,
+                       weight float ,
+                       marital_status varchar(10), 
+                        adhar_no bigint ,
+                       pan_no varchar(10));
+
+CREATE TABLE my_sister(name varchar(20) , 
+                       age int , 
+                       occupation varchar(20) ,
+                       mobileNo bigint , 
+                       gmail varchar(30),
+                       height float ,
+                       weight float ,
+                       marital_status varchar(10) ,
+                        adhar_no bigint ,
+                       pan_no varchar(10));
+                       
+CREATE TABLE my_brother(name varchar(20) , 
+                       age int , 
+                       occupation varchar(20) ,
+                       mobileNo bigint , 
+                       gmail varchar(30),
+                       height float ,
+                       weight float ,
+                       marital_status varchar(10) ,
+                        adhar_no bigint ,
+                       pan_no varchar(10));
+                       
+CREATE TABLE my_uncle(name varchar(20) , 
+                       age int , 
+                       occupation varchar(20) ,
+                       mobileNo bigint , 
+                       gmail varchar(30),
+                       height float ,
+                       weight float ,
+                       marital_status varchar(10) ,
+                        adhar_no bigint ,
+                       pan_no varchar(10));
+                       
+CREATE TABLE my_aunty(name varchar(20) , 
+                       age int , 
+                       occupation varchar(20) ,
+                       mobileNo bigint , 
+                       gmail varchar(30),
+                       height float ,
+                       weight float ,
+                       marital_status varchar(10) ,
+                        adhar_no bigint ,
+                       pan_no varchar(10));
+                       
+CREATE TABLE my_cousin_brother(name varchar(20) , 
+                       age int , 
+                       occupation varchar(20) ,
+                       mobileNo bigint , 
+                       gmail varchar(30),
+                       height float ,
+                       weight float ,
+                       marital_status varchar(10) ,
+                        adhar_no bigint ,
+                       pan_no varchar(10));
+
+CREATE TABLE my_cousin_sister(name varchar(20) , 
+                       age int , 
+                       occupation varchar(20) ,
+                       mobileNo bigint , 
+                       gmail varchar(30),
+                       height float ,
+                       weight float ,
+                       marital_status varchar(10) ,
+                        adhar_no bigint ,
+                       pan_no varchar(10));
+                       
+CREATE TABLE my_nephew(name varchar(20) , 
+                       age int , 
+                       occupation varchar(20) ,
+                       mobileNo bigint , 
+                       gmail varchar(30),
+                       height float ,
+                       weight float ,
+                       marital_status varchar(10) ,
+                        adhar_no bigint ,
+                       pan_no varchar(10));
+                       
+CREATE TABLE my_niece(name varchar(20) , 
+                       age int , 
+                       occupation varchar(20) ,
+                       mobileNo bigint , 
+                       gmail varchar(30),
+                       height float ,
+                       weight float ,
+                       marital_status varchar(10) ,
+                        adhar_no bigint ,
+                       pan_no varchar(10));                                              
+                                              
+                                              
+                       
+                       
+                       
+					
